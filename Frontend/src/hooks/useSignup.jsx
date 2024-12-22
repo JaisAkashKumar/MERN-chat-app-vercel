f139c17cc1865a00cd780257f37b48899c383954
Akash Kumar <akashjaiswal7301@gmail.com>
2024-12-22 15:34:58 +0530

    updated

--- Frontend/src/hooks/useSignup.jsx
+++ Frontend/src/hooks/useSignup.jsx
@@ -22,17 +22,20 @@ const useSignup = () => {
     if (!success) return;
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/signup", {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({
-          fullName,
-          username,
-          password,
-          confirmPassword,
-          gender,
-        }),
-      });
+      const res = await fetch(
+        "https://akash-chat-app.vercel.app/api/auth/signup",
+        {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify({
+            fullName,
+            username,
+            password,
+            confirmPassword,
+            gender,
+          }),
+        }
+      );
       const data = await res.json();
       if (data.error) {
         throw new Error(data.error);
